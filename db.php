cc4488987b3573ce38a500968bed84a93b121bec
JCanode1 <josh@tarrs.com>
2023-12-16 21:56:42 -0700

    .

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+ini_set('display_errors', 1);
 
 
 $serverName = "jokesdb-server.mysql.database.azure.com";
@@ -10,56 +10,13 @@ $username = "ghitbyareg";
 $password = "Password1!"; // Replace with your actual database password
 
 // Create connection
-$conn = new mysqli($serverName, $username, $password);
+$conn = new mysqli($serverName, $username, $password, $databaseName);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully to the MySQL server.";
-
-
-// Create the database
-$sql = "CREATE DATABASE IF NOT EXISTS $databaseName";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-
-// Select the database
-$conn->select_db($databaseName);
-
-// Create the Users table
-$sql = "CREATE TABLE IF NOT EXISTS Users (
-    UserID INT AUTO_INCREMENT PRIMARY KEY,
-    Username VARCHAR(50) NOT NULL UNIQUE,
-    PasswordHash VARCHAR(255) NOT NULL,
-    google2fa_secret VARCHAR(255),
-    is_2fa_enabled TINYINT(1) DEFAULT 0
-)";
-if ($conn->query($sql) === TRUE) {
-    echo "Table Users created successfully";
-} else {
-    echo "Error creating table Users: " . $conn->error;
-}
-
-// Create the Jokes table
-$sql = "CREATE TABLE IF NOT EXISTS Jokes (
-    JokeID INT AUTO_INCREMENT PRIMARY KEY,
-    UserID INT,
-    JokeText TEXT NOT NULL,
-    AnswerText TEXT NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
-)";
-if ($conn->query($sql) === TRUE) {
-    echo "Table Jokes created successfully";
-} else {
-    echo "Error creating table Jokes: " . $conn->error;
-}
-
-// Close the connection
-$conn->close();
+// You can use this $conn object for executing SQL queries in your application.
 
 ?>

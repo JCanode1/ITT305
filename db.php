f12e26ebe55c54418e1f230ba58f4991ab247ffc
JCanode1 <josh@tarrs.com>
2023-12-16 22:00:30 -0700

    more changes to db.php

--- db.php
+++ db.php
@@ -1,22 +1,22 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
 $serverName = "jokesdb-server.mysql.database.azure.com";
 $databaseName = "jokes_DB";
 $username = "ghitbyareg";
-$password = "Password1!"; // Replace with your actual database password
+$password = "Password1!";
 
-// Create connection
-$conn = new mysqli($serverName, $username, $password, $databaseName);
+// Adjust the paths based on the location of your PHP scripts
+$sslOptions = array(
+    'ssl' => array(
+        'cafile' => __DIR__ . '/ca-cert.pem',
+        'certfile' => __DIR__ . '/client-cert.pem',
+        'keyfile' => __DIR__ . '/client-key.pem',
+        'verify_peer' => true,
+    ),
+);
 
-// Check connection
+$conn = new mysqli($serverName, $username, $password, $databaseName, 3306, null, $sslOptions);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// You can use this $conn object for executing SQL queries in your application.
-
 ?>
